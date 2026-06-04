8fb10ca01ad871c465c5ce394f0f74a80c1b4b06
Miruthula <miruthulak21@gmail.com>
2026-06-04 11:32:36 +0530

    Add Kiruthika to report recipients

--- Desktop/Web_ReKYC_Test/Web_ReKYC_Config.py
+++ Desktop/Web_ReKYC_Test/Web_ReKYC_Config.py
@@ -24,7 +24,7 @@ EMAIL_REPORT = {
     "sender"      : "aialerts@navia.co.in",
     "username"    : "emailapikey",
     "password"    : "PHtE6r1eS7jqiG998kUH7afqRZKmN4gtrrw1KQQTt4sTDfJRS01U+d8qlTCwqU0sAPJCRqHKmY1p4rqb4e+Ed26/YW8ZDWqyqK3sx/VYSPOZsbq6x00auVwYdELbVIXqe9di0CzRst3YNA==",
-    "receiver"    : ["miruthulak21@gmail.com", "elamukil@navia.co.in"],
+    "receiver"    : ["miruthulak21@gmail.com", "elamukil@navia.co.in", "kiruthika@navia.co.in"],
     "smtp_server" : "smtp.zatpatmail.com",
     "smtp_port"   : 465
 }
